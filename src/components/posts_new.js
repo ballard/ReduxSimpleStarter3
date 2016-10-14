4032fcc22a2f2943a04af8175e9f092e86299a56
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-14 17:48:47 +0300

    section 6 part 86 complete (routes example)

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,44 +3,49 @@
  */
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class  PostsNew extends Component {
     render() {
         const { fields: {title, categories, content}, handleSubmit } = this.props;
-        console.log(title);
-
         //equals to
         // const title = this.props.fields.title;
         // etc...
         //const handleSubmit = this.props.handleSubmit;
+
+        // below {...title} is extraction from props
+
         return (
-            <form onSubmit={handleSubmit}>
-                <h3>Create a new Post</h3>
+            <form onSubmit={handleSubmit(this.props.createPost)}>
+                 <h3>Create a new Post</h3>
 
-                <div className="form-group">
-                    <label>Title</label>
-                    <input type="text" className="form-control" {...title}/> //{...title} is extraction from props
-                </div>
+                 <div className="form-group">
+                     <label>Title</label>
+                     <input type="text" className="form-control" {...title}/>
+                 </div>
 
-                <div className="form-group">
-                    <label>Categories</label>
-                    <input type="text" className="form-control" {...categories}/>
-                </div>
+                 <div className="form-group">
+                     <label>Categories</label>
+                     <input type="text" className="form-control" {...categories}/>
+                 </div>
 
-                <div className="form-group">
-                    <label>Content</label>
-                    <textarea type="text" className="form-control" {...content}/>
-                </div>
+                 <div className="form-group">
+                     <label>Content</label>
+                     <textarea type="text" className="form-control" {...content}/>
+                 </div>
 
-                <button type="submit" className="btn button-primary">Submit</button>
+                 <button type="submit" className="btn button-primary">Submit</button>
             </form>
         );
     };
 }
 
+// connect: 1 mapStateToProps, 2 mapDispatchToProps
+// reduxForm 1 form config, 2 mapStateToProps, 3 mapDispatchToProps
+
 export default reduxForm({
     form: 'PostsNewForm',
     fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
 
 
