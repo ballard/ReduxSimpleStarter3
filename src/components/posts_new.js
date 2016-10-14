b7f5142b984534275728b4aec2b21cd8f0525d80
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-14 17:00:45 +0300

    section 6 part 84 complete (routes example)

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,13 +2,37 @@
  * Created by ivanlazarev on 14.10.16.
  */
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class  PostsNew extends Component {
     render() {
         return (
-            <div>create form</div>
+            <form>
+                <h3>Create a new Post</h3>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control"/>
+                </div>
+
+                <div className="form-group">
+                    <label>Categories</label>
+                    <input type="text" className="form-control"/>
+                </div>
+
+                <div className="form-group">
+                    <label>Content</label>
+                    <textarea type="text" className="form-control"/>
+                </div>
+
+                <button type="submit" className="btn button-primary">Submit</button>
+            </form>
         );
     };
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content']
+})(PostsNew);
+
+
