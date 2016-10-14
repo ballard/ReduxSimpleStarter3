223baada66c05902d124e3f28c94003a316a1fb2
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-14 15:07:47 +0300

    section 6 part 75 complete (routes example)

---
+++ src/components/posts_index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ivanlazarev on 14.10.16.
+ */
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            list of blog posts
+        </div>
+    );
+};
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
 const Greeting = () => {
     return <div>Hello there!</div>;
@@ -13,6 +14,7 @@ const Greeting = () => {
 
 export default (
     <Route path="/" component={App}>
+        <IndexRoute component={PostsIndex}/>
         <Route path="greet" component={Greeting}/>
         <Route path="greet2" component={Greeting}/>
         <Route path="greet3" component={Greeting}/>
