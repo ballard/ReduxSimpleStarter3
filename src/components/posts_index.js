f5fcbcbc5bd9547acadba8dbafe64d171c199cd7
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-14 15:38:55 +0300

    section 6 part 79 complete (routes example)

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,12 +1,18 @@
 /**
  * Created by ivanlazarev on 14.10.16.
  */
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return (
-        <div>
-            list of blog posts
-        </div>
-    );
-};
+class PostsIndex extends Component {
+    componentWillMount() {  //called just before component appears - lifecycle method
+        console.log('calling action creator');
+    }
+
+    render() {
+        return (
+            <div>list of posts</div>
+        );
+    };
+}
+
+export default PostsIndex;
