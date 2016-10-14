b67d1e2f511b9af4efea65e36b197587db7aa0b7
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-14 18:40:04 +0300

    section 6 part 90 complete (routes example)

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -13,6 +13,18 @@ class PostsIndex extends Component {
         this.props.fetchPosts();
     }
 
+    renderPosts() {
+        return this.props.posts.map((post) => {
+            return (
+            <li className="list-group-item" key={post.id}>
+                <span className="pull-xs-right">{post.categories}</span>
+                <strong>{post.title}</strong>
+            </li>
+            );
+        });
+    }
+
+
     render() {
         return (
             <div>
@@ -21,13 +33,21 @@ class PostsIndex extends Component {
                         Add a Post
                     </Link>
                 </div>
-                list of posts
+
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     };
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+    return { posts: state.posts.all };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
 
 //JSX syntax rules!!!
 //equals to
