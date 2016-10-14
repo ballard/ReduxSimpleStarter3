d38b676399022af56d59b9e3e2cc15c247a79fb8
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-14 15:51:39 +0300

    section 6 part 80 complete (routes example)

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,10 +2,15 @@
  * Created by ivanlazarev on 14.10.16.
  */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
+
 
 class PostsIndex extends Component {
-    componentWillMount() {  //called just before component appears - lifecycle method
+    componentWillMount() {  //called just before component appears on the DOM - lifecycle method
         console.log('calling action creator');
+        this.props.fetchPosts();
     }
 
     render() {
@@ -15,4 +20,15 @@ class PostsIndex extends Component {
     };
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
+
+//JSX syntax rules!!!
+//equals to
+// export default connect(null, { fetchPosts: fetchPosts } )(PostsIndex);
+//equals to
+//
+// function mapDispatchToProps(dispatch) {
+//     return bindActionCreators({fetchPosts}, dispatch);
+// }
+//
+// export default connect(null, mapDispatchToProps)(PostsIndex);
