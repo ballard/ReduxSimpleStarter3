250dac090b22b3e8bdc11e3ee0ae919239543aa9
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-14 14:39:13 +0300

    section 6 part 73 complete

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ivanlazarev on 14.10.16.
+ */
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+
+export default (
+    <Route path="/" component={App}/>
+);
+
