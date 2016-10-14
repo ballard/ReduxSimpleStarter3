038ca386dc52075abd386c471374fe0fc5790c24
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-14 17:21:17 +0300

    section 6 part 85 complete (routes example)

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -6,22 +6,30 @@ import { reduxForm } from 'redux-form';
 
 class  PostsNew extends Component {
     render() {
+        const { fields: {title, categories, content}, handleSubmit } = this.props;
+        console.log(title);
+
+        //equals to
+        // const title = this.props.fields.title;
+        // etc...
+        //const handleSubmit = this.props.handleSubmit;
         return (
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h3>Create a new Post</h3>
+
                 <div className="form-group">
                     <label>Title</label>
-                    <input type="text" className="form-control"/>
+                    <input type="text" className="form-control" {...title}/> //{...title} is extraction from props
                 </div>
 
                 <div className="form-group">
                     <label>Categories</label>
-                    <input type="text" className="form-control"/>
+                    <input type="text" className="form-control" {...categories}/>
                 </div>
 
                 <div className="form-group">
                     <label>Content</label>
-                    <textarea type="text" className="form-control"/>
+                    <textarea type="text" className="form-control" {...content}/>
                 </div>
 
                 <button type="submit" className="btn button-primary">Submit</button>
