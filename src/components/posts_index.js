17e646a69cf7d706113a2ae6475e3cb830fb4f93
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-14 16:41:26 +0300

    section 6 part 82 complete (routes example)

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 
 class PostsIndex extends Component {
@@ -14,7 +15,14 @@ class PostsIndex extends Component {
 
     render() {
         return (
-            <div>list of posts</div>
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add a Post
+                    </Link>
+                </div>
+                list of posts
+            </div>
         );
     };
 }
